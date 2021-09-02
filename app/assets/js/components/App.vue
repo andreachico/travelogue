1150767b3bf73b57fb53dc4dde13e31eed5fec61
Andrea Chico <55386799+andreachico@users.noreply.github.com>
2021-09-02 14:30:29 +0200

    Initial commit

---
+++ app/assets/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2 class="text-center text-light">You did it! This page is rendered by vue.</h2>
+    <h4 class="text-center">Happy coding!</h4>
+    <h4 class="text-center">:-)</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
---
+++ app/assets/js/vanilla.js
@@ -0,0 +1,1 @@
+console.log('vanilla JS is working. Woohoo!');
