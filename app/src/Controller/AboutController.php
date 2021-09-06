ae5e4829fb674640c3bd734e68686db66dacbaa2
Andrea Chico <andymaechico@gmail.com>
2021-09-06 12:27:30 +0200

    add about content

--- app/src/Controller/AboutController.php
+++ app/src/Controller/AboutController.php
@@ -7,6 +7,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AboutController extends AbstractController {
+class AboutController extends AbstractController
+{
+    /**
+     * @Route("/about", name="about")
+     * @throws Exception
+     */
+    public function about(): Response
+    {
+        $title = 'TRAVELOGUE';
+        $slogan = 'Traveling opens door to creating memories!';
 
+        return $this->render('about/about.html.twig', [
+            'title' => $title,
+            'slogan' => $slogan,
+        ]);
+    }
 }
