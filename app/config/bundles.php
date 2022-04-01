f090df7017452d420bf8bffebfac3010329c2e40
Andrea Chico <andymaechico@gmail.com>
2022-04-01 11:35:13 +0200

    new bundles

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
